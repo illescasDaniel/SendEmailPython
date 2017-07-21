ab842a1821d8beff02d842ec8b2112dfbb6316c7
illescasDaniel <illescas.daniel@protonmail.com>
2017-07-21 02:53:57 +0200

    Now user can input SMTP and server port Some settings are saved on disk now

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SendEmailPython
 
-[![Version](https://img.shields.io/badge/version-v1.0.1-green.svg)](https://github.com/illescasDaniel/SendEmailPython/releases)
+[![Version](https://img.shields.io/badge/version-v1.1-green.svg)](https://github.com/illescasDaniel/SendEmailPython/releases)
 [![license](https://img.shields.io/github/license/mashape/apistatus.svg?maxAge=2592000)](https://github.com/illescasDaniel/SendEmailPython/blob/master/LICENCE)
 
 A GUI program to send emails using the `smtplib` Python library and `tkinter` for the UI.
--- src/SendEmail.py
+++ src/SendEmail.py
@@ -15,10 +15,18 @@ class Application(Frame):
 	senderEntry = Entry
 	senderPasswordLabel = Label
 	senderPasswordEntry = Entry
+	SMTPServerLabel = Label
+	SMTPServerEntry = Entry
+	SMTPServerPortLabel = Label
+	SMTPServerPortEntry = Entry
 	subjectLabel = Label
 	subjectEntry = Entry
 	receiverLabel = Label
 	receiverEntry = Entry
+
+	savedSenderEmail = str()
+	savedServerAddress = str()
+	savedServerPort = str()
 
 	emailContentLabel = Label
 	emailContentBox = Text
@@ -30,6 +38,7 @@ class Application(Frame):
 		if sys.version_info < (3, 0): Frame.__init__(self, master, border = 12)
 		else: super().__init__(master, border = 15)
 
+		self.readSettings()
 		self.pack(fill = BOTH, expand = True)
 		self.create_widgets()
 
@@ -41,15 +50,31 @@ class Application(Frame):
 		userInfoLabelFrame = LabelFrame(loginFrame, text = "User info", pady = 4)
 		userInfoLabelFrame.grid(row = 0, sticky = E+W)
 
-		self.senderLabel = Label(userInfoLabelFrame, text = "Sender:")
+		senderEmail = "" if not self.savedSenderEmail else self.savedSenderEmail
+		defaultSenderEmail = StringVar(userInfoLabelFrame, value = senderEmail)
+		self.senderLabel = Label(userInfoLabelFrame, text = "Sender Email:")
 		self.senderLabel.grid(row = 0, column = 0)
-		self.senderEntry = Entry(userInfoLabelFrame)
+		self.senderEntry = Entry(userInfoLabelFrame, textvariable = defaultSenderEmail)
 		self.senderEntry.grid(row = 0, column = 1)
 
 		self.senderPasswordLabel = Label(userInfoLabelFrame, text = "Password:")
 		self.senderPasswordLabel.grid(row = 1, column = 0)
 		self.senderPasswordEntry = Entry(userInfoLabelFrame, show = "*")
 		self.senderPasswordEntry.grid(row = 1, column = 1)
+
+		serverAddress = "smtp.EMAIL.com" if not self.savedServerAddress else self.savedServerAddress
+		defaultServerAddress = StringVar(userInfoLabelFrame, value = serverAddress)
+		self.SMTPServerLabel = Label(userInfoLabelFrame, text = "SMTP Server:")
+		self.SMTPServerLabel.grid(row = 2, column = 0)
+		self.SMTPServerEntry = Entry(userInfoLabelFrame, textvariable = defaultServerAddress)
+		self.SMTPServerEntry.grid(row = 2, column = 1)
+
+		serverPort = "587" if not self.savedServerPort else self.savedServerPort
+		defaultServerPort = StringVar(userInfoLabelFrame, value = serverPort)
+		self.SMTPServerLabel = Label(userInfoLabelFrame, text = "Server Port:")
+		self.SMTPServerLabel.grid(row = 3, column = 0)
+		self.SMTPServerPortEntry = Entry(userInfoLabelFrame, textvariable = defaultServerPort)
+		self.SMTPServerPortEntry.grid(row = 3, column = 1)
 
 		emailInfoLabelFrame = LabelFrame(loginFrame, text = "Email", pady = 4)
 		emailInfoLabelFrame.grid(row = 1, sticky = E+W)
@@ -80,14 +105,17 @@ class Application(Frame):
 		subject = str(self.subjectEntry.get())
 		receiver = str(self.receiverEntry.get())
 		mailContent = str(self.emailContentBox.get("1.0",'end-1c'))
+		serverAddress = str(self.SMTPServerEntry.get())
+		serverPort = str(self.SMTPServerPortEntry.get())
 
-		if not sender or not password or not subject or not receiver or not mailContent:
+		if not sender or not password or not subject or not receiver or not mailContent or not serverAddress or not serverPort:
 			messagebox.showerror(title = "Error", message = "Some fields are empty")
 		else:
+			self.saveSettings(sender, serverAddress, serverPort)
 			message = "To: {0}\nFrom: {1}\nSubject:{2}\n\n{3}".format(sender, receiver, subject, mailContent)
 
 			try:
-				smtpServer = SMTP("smtp.gmx.com", 587) # You need to change this address
+				smtpServer = SMTP(serverAddress, serverPort) # You need to change this address
 				smtpServer.ehlo()
 				smtpServer.starttls()
 				smtpServer.ehlo()
@@ -99,12 +127,29 @@ class Application(Frame):
 			except SMTPException:
 				messagebox.showerror(title = "Error", message = "Unable to send email")
 
+	def readSettings(self):
+		with open("settings", "a+") as file:
+			file.seek(0)
+			self.savedSenderEmail = file.readline().strip("\n")
+			self.savedServerAddress = file.readline().strip("\n")
+			self.savedServerPort = file.readline()
+			file.truncate()
+			file.close()
+
+	def saveSettings(self, senderEmail, serverAddressToSave, serverPortToSave):
+		with open("settings", "r+") as file:
+			file.seek(0)
+			file.write(senderEmail + "\n")
+			file.write(serverAddressToSave + "\n")
+			file.write(serverPortToSave)
+			file.truncate()
+			file.close()
+
 if __name__ == '__main__':
 
 	mainWindow = Tk()
 	mainWindow.title("Send Email")
 	mainWindow.config(background = "green")
-	#mainWindow.resizable(width = False, height = False)
 	app = Application(master = mainWindow)
 	mainWindow.update()
 	mainWindow.minsize(mainWindow.winfo_width(), mainWindow.winfo_height())
